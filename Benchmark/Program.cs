dd37cff50be1766ca8ef44f8f7e21cb4463dd8e8
Thiago Borba <thiago.borba@cwi.com.br>
2021-04-03 23:13:28 -0300

    Alterado assinatura do IValidadorStrategy para receber o registro corrente (current) e próximo (next) para tratar batidas noturnas.

---
+++ Benchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+using BenchmarkDotNet.Running;
+using PoCMotorRegraPontoTests.Registros;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var summary = BenchmarkRunner.Run(typeof(RegistroTests).Assembly);
+
+            var a = 2;
+
+            var r = (a ^ 1) == a + 1;
+
+            Console.WriteLine(r);
+        }
+    }
+}
